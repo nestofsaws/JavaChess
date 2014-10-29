3ae050303f13783fb6212a017ad416b4e4d0d6e3
Brian Zimorowicz <zims@nestofsaws.com>
2014-10-29 09:25:10 -0400

    Create script.java

---
+++ script.java
@@ -0,0 +1,17 @@
+package a7;
+
+public class script {
+
+	public script() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
